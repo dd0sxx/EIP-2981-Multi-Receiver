3290cf43deb4d3c197c57a3585639d3f3d0a7665
dd0sxx <contacttheotelonis@gmail.com>
2022-01-05 17:12:25 -0500

    process

--- contracts/EIP_2981_Multi_Receiver.sol
+++ contracts/EIP_2981_Multi_Receiver.sol
@@ -7,20 +7,38 @@ import "hardhat/console.sol";
 
 contract EIP2981_Multi_Receiver {
 
+    bool internal locked;
+
+    modifier reentrancyGuard() {
+        require(!locked, 'reentrancy guard locked');
+        locked = true;
+        _;
+        locked = false;
+    }
+
     struct receiver {
-        address wallet;
+        address payable wallet;
         uint8 percent;
     }
 
+    address[] receiversArray;
 
-    receiver[] receivers;
+    mapping(address => uint8) receiversMap;
    
     constructor(receiver[] memory list) {
-        receivers = list;
+        for (uint i; i < list.length; i++) {
+            receiversMap[list[i].wallet] = list[i].percent;
+            receiversArray.push(list[i].wallet);
+        }
     }
 
-    receive () external payable {
-        
+    receive () external payable reentrancyGuard {
+        for(uint i; i < receiversArray.length; i++) {
+            address payable addressTemp = payable(receiversArray[i]);
+            uint portion = (msg.value * 100) / receiversMap[addressTemp];
+            (bool status, ) = addressTemp.call{value: portion}("");
+            require(status == true, 'withdraw failed');
+        }
     }
 
 }
