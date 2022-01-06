e7e00086a5c62ec202fd3cfa45732eb20d277e9e
dd0sxx <contacttheotelonis@gmail.com>
2022-01-06 13:29:24 -0500

    receivers are being set correctly

--- test/multi-receiver-test.js
+++ test/multi-receiver-test.js
@@ -3,6 +3,7 @@ const { ethers } = require("hardhat")
 
 describe("multi-receiver-test", function () {
   let alice,bob,charlotte
+  let Mr
 
   beforeEach (async () => {
     [a, b, c] = await ethers.getSigners()
@@ -10,13 +11,20 @@ describe("multi-receiver-test", function () {
     bob = b
     charlotte = c
     const MR = await ethers.getContractFactory("EIP2981_Multi_Receiver")
-    const Mr = await MR.deploy([{wallet: alice.address, percent: 50}, {wallet: alice.address, percent: 30}, {wallet: alice.address, percent: 20}])
+    Mr = await MR.deploy([{wallet: alice.address, percent: 50}, {wallet: bob.address, percent: 30}, {wallet: charlotte.address, percent: 20}])
     await Mr.deployed()
 
   })
 
   it("should create a list of receivers", async function () {
+    expect(await Mr.isReceiver(alice.address)).to.deep.equal(true)    
+    expect(await Mr.isReceiver(bob.address)).to.deep.equal(true)    
+    expect(await Mr.isReceiver(charlotte.address)).to.deep.equal(true)    
+  })
 
-    
+  it("should create a list of receivers", async function () {
+    expect(await Mr.getPercentage(alice.address)).to.deep.equal(50)    
+    expect(await Mr.getPercentage(bob.address)).to.deep.equal(30)    
+    expect(await Mr.getPercentage(charlotte.address)).to.deep.equal(20)    
   })
 })
